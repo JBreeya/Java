e07e5fe333cb854f2fcfea047309bdc9093d3c6b
Joseline Breeya <138107378+JBreeya@users.noreply.github.com>
2023-09-01 12:42:56 +0530

    Add files via upload

---
+++ HeYThereEarth.java
@@ -0,0 +1,7 @@
+class HeyThereEarth
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hey there, Earth!");
+	}
+}
---
+++ welcome.java
@@ -0,0 +1,7 @@
+class welcome
+{
+	public static void main(String agrs[ ])
+	{
+		System.out.println("Welcome");
+	}
+}
